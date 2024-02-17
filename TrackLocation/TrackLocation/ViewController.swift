1aa9fb5eb5acd636c1fb36369ec9c3a2f6500c54
haykct <https://github.com/HaykoHayrapetyan>
2024-02-17 19:30:57 +0100

    Added project.

---
+++ TrackLocation/TrackLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackLocation
+//
+//  Created by Hayk Hayrapetyan on 17.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
